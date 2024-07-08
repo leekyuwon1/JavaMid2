d400fc97507480cf229b9c95d4f7bd560e0abaef
leekyuwon <sjin9574@naver.com>
2024-07-08 23:32:23 +0900

    JavaMiddleLearning

---
+++ src/collection/list/BatchProcessor.java
@@ -0,0 +1,22 @@
+package collection.list;
+
+public class BatchProcessor {
+//    private final MyLinkedList<Integer> list = new MyLinkedList<>(); // 코드 변경 ( OCP 원칙 )
+    private final MyList<Integer> list;
+
+    // MyList = new MyLinkedList( 자식 )
+    // MyList = new MyArrayList( 자식 )
+    public BatchProcessor(MyList<Integer> list) {
+        this.list = list;
+    }
+
+    public void logic(int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(0, i); // 앞에 추가
+//            list.add(i); // 끝에 추가
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("크기: " + size + ", 계산 시간: " + (endTime - startTime) + "ms");
+    }
+}
