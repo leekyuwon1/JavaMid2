7c860ef3ccca50f9f1f1eef4371a3a436650ce02
leekyuwon <sjin9574@naver.com>
2024-08-01 10:27:24 +0900

    JavaMiddleLearning

---
+++ src/collection/compare/test/Deck.java
@@ -0,0 +1,36 @@
+package collection.compare.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+
+    private List<Card> cards = new ArrayList<>();
+
+    public Deck() {
+        initCard();
+        shuffle();
+    }
+
+    private void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    private void initCard() {
+        for (int i = 1; i <= 13; i++) {
+            for (Suit suit : Suit.values()) {
+                cards.add(new Card(i, suit));
+            }
+//            cards.add(new Card(i, Suit.SPADE));
+//            cards.add(new Card(i, Suit.HEART));
+//            cards.add(new Card(i, Suit.DIA));
+//            cards.add(new Card(i, Suit.CLOVER));
+        }
+    }
+
+    public Card drawCard() {
+        return cards.remove(0);
+//        return cards.removeFirst() - 자바 21 사용가능
+    }
+}
